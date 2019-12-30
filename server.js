d71ea63985c10ba0c9388586e8ce2bd01f39451e
PraveenMatthewPhilip <58952791+PraveenMatthewPhilip@users.noreply.github.com>
2019-12-30 21:05:15 +0530

    functionality for adding user added

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+connectDB();
+
+//init middleware
+
+app.use(express.json({extended: false}))
 
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
