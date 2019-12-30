e5388a6554f24e3b2fbe9efa6a5dad8e4c6df1d2
PraveenMatthewPhilip <58952791+PraveenMatthewPhilip@users.noreply.github.com>
2019-12-30 20:13:10 +0530

    Express server setup with inital setup

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'))
+app.get('/', (req, res) => res.json({msg: 'Welcome to the ContactKeeper API'}))
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
